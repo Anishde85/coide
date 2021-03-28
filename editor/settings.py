2fcde44dc9f54264252e76bbf6d628fb22c9fdac
anishde <anishde85@gmail.com>
2021-03-28 14:33:36 +0530

    whitenoise

--- editor/settings.py
+++ editor/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

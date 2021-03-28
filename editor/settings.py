92f2790db90f8bbe65deda307a2f22162e9db4f6
anishde <anishde85@gmail.com>
2021-03-28 13:12:57 +0530

    code editor

--- editor/settings.py
+++ editor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iq6bq03ayisv)833p4*6vaw+c9w3g+=v4hv$*n9v%=b!l2%59^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coide.herokuapp.com']
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
